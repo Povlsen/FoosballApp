4ad71ebbd87bfafab057f5b835cdf126bdbe7c0e
Emil Neirup Jensen <emilnjensen@gmail.com>
2021-03-23 12:49:44 +0100

    Added pics to src dir

--- src/components/TopView/index.js
+++ src/components/TopView/index.js
@@ -1,13 +1,43 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import './TopView.scss'
+import Emil from '../../assets/emil.jpg'
 
-class TopView extends Component {
-    render(){
-        return (
-            <div class="view">
+const TopView = props => (
+    <div class='view'>
+        <p class="heading">{props.headline}</p>
+        <div class='podium'>
+            <div class='podium-item'>
+                <div class='podium-item-1-top'>
+                    <img class="pic" src={Emil}/>
+                    <p>Emil</p>
+                </div>
+                <div class='podium-item-1'>
+                    <p>17 wins</p>
+                    <p>64%</p>
+                </div>
             </div>
-        )
-    }
-}
+            <div class='podium-item'>
+                <div class='podium-item-2-top'>
+                    <img class="pic" src={Emil}/>
+                    <p>Emil</p>
+                </div>
+                <div class='podium-item-2'>
+                    <p>17 wins</p>
+                    <p>64%</p>
+                </div>
+            </div>
+            <div class='podium-item'>
+                <div class='podium-item-3-top'>
+                    <img class="pic" src={Emil}/>
+                    <p>Emil</p>
+                </div>
+                <div class='podium-item-3'>
+                    <p>17 wins</p>
+                    <p>64%</p>
+                </div>
+            </div>
+        </div>
+    </div>
+)
 
 export default TopView

f95888985ab15c9cfb0c7eccab1363f0e6195dda
Nikolai R. T. Povlsen <nikolaipovlsen@yahoo.dk>
2021-03-25 23:33:06 +0100

    Pushy push updated form adduser

--- src/actions/Players.js
+++ src/actions/Players.js
@@ -23,6 +23,6 @@ export const getPlayerScoreDetails = playerId => {
 export const postPlayer = data => {
     console.log('player/put', `{"playerId": 0, "firstName": '${data.firstname}', "lastName": '${data.lastname}', "displayName": '${data.displayname}', "profilePicture": "string"}`)
     return dispatch => {
-        return axios.put('player/put', `{"playerId": 0, "firstName": "${data.firstname}", "lastName": "${data.lastname}", "displayName": "${data.displayname}"}`);
+        return axios.put('player', data);
     }
 }

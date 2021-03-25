4186219fcd5c922347ebff7276dd91826de571f7
Emil Neirup Jensen <emilnjensen@gmail.com>
2021-03-25 20:04:21 +0100

    Added add user form ui

--- src/components/AddUser/index.js
+++ src/components/AddUser/index.js
@@ -1,19 +1,34 @@
 import React, { useEffect, useState } from 'react'
 import { connect } from 'react-redux'
 import ReactModal from 'react-modal'
+import './AddUser.scss'
 
 const AddUser = () => {
     const [showAddTeamModal, setShowAddTeamModal] = useState(false)
         
     return( 
         <>
-        {console.log(showAddTeamModal)}
-            <button onClick={_ => setShowAddTeamModal(true)}>Manage Team</button>
+            <button onClick={_ => setShowAddTeamModal(true)}>Add user</button>
         
-            <ReactModal className={'Overlay'} isOpen={showAddTeamModal} onRequestClose={_ => setShowAddTeamModal(false)}
+            <ReactModal 
+                className={'Overlay'} 
+                isOpen={showAddTeamModal} 
+                onRequestClose={_ => setShowAddTeamModal(false)}
             >
                 <div class='modal-view' >
+                    <div class='close-btn' onClick={_ => setShowAddTeamModal(false)}>Close</div>
                     <p>Add User</p>
+                    <form class='user-form'>
+                        <p class='lable'>Firstname:</p>
+                        <input type='text' class='input'></input>
+                        <p class='lable'>Lastname:</p>
+                        <input type='text' class='input'></input>
+                        <p class='lable'>Displayname:</p>
+                        <input type='text' class='input'></input>
+                        <p class='lable'>Image:</p>
+                        <input type='file' class='input'></input>
+                        <button class='form-submit'>Add user:</button>
+                    </form>
                 </div>
             </ReactModal>
         </>

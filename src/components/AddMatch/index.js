cd6fd4d94d186cfae42690cbbffe29c196774b4c
Mikkel <mikkel@norre.cc>
2021-03-25 12:43:01 +0100

    removed commented code

--- src/components/AddMatch/index.js
+++ src/components/AddMatch/index.js
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
-import ReactModal from 'react-modal';
+import React, { useEffect, useState } from 'react'
+import { connect } from 'react-redux'
+import ReactModal from 'react-modal'
 
+import { getPlayers } from '../../actions/Players'
 import './AddMatch.scss'
 
 import Player1 from '../../assets/Forfatter.png'
@@ -11,14 +13,24 @@ import Player4 from '../../assets/Nikolai.png'
 import Team from './Team';
 import Selector from './PlayerSelector'
 
-var teamOnePoints = 0;
-var teamTwoPoints = 0;
+const AddTeam = ({ handleClose, show, getPlayers }) => {
+    const [teamOne, setTeamOne] = useState([])
+    const [teamTwo, setTeamTwo] = useState([])
+    const [teamOnePoints, setTeamOnePoints] = useState(0)
+    const [teamTwoPoints, setTeamTwoPoints] = useState(0)
 
-var selectedUsers = [51,52,321,46];
+    useEffect(_ => {
+        getPlayers()
+    }, [getPlayers])
 
-const AddTeam = ({handleClose, show, children }) => {
+    useEffect(_ => {
+        const getSum = list => list.reduce((val, item) => val + (isNaN(item.goalsFrom) ? 0 : item.goalsFrom), 0)
+        setTeamOnePoints(getSum(teamOne))
+        setTeamTwoPoints(getSum(teamTwo))
+    }, [teamOne, teamTwo])
+
     if (show != true)
-        return null;
+        return null
 
     return (
         <ReactModal 
@@ -27,14 +39,14 @@ const AddTeam = ({handleClose, show, children }) => {
             className={'greyOverlay'}
             contentElement={(props, children) => <div {...props} onClick={handleClose}>{children}</div>}
             >
-            <div id='background' onClick={e => e.stopPropagation()}>
+            <div class='content' onClick={e => e.stopPropagation()}>
                 <h3 id='title'>New Match</h3>
                 <div class='pointGrid'>
                     <div>
-                        <div id='teamOnePoints'><p>{teamOnePoints}</p></div>
+                        <div><p>{teamOnePoints}</p></div>
                     </div>
                     <div>
-                        <div id='teamTwoPoints'><p>{teamTwoPoints}</p></div>
+                        <div><p>{teamTwoPoints}</p></div>
                     </div>
                 </div>
                 <div class="selectTeamGrid">
@@ -74,94 +86,17 @@ const AddTeam = ({handleClose, show, children }) => {
                     </div>
                 </div>
                 <div class='buttonHolder'>
-                    <button onClick={sendMatch}>Add</button>
+                    <button onClick={_ => {}}>Add</button>
                 </div>
             </div>
         </ReactModal>
-    );
-    /*return (
-        <ReactModal 
-            isOpen={show} 
-            onRequestClose={handleClose} 
-            className={'greyOverlay'}
-            contentElement={(props, children) => <div {...props} onClick={handleClose}>{children}</div>}
-            >
-            <div id='background' onClick={e => e.stopPropagation()}>
-                <h3 id='title'>New Match</h3>
-                <div class='pointGrid'>
-                    <div>
-                        <div id='teamOnePoints'><p>{teamOnePoints}</p></div>
-                    </div>
-                    <div>
-                        <div id='teamTwoPoints'><p>{teamTwoPoints}</p></div>
-                    </div>
-                </div>
-                <div class="selectTeamGrid">
-                    <div>
-                        <img src={Player1}></img>
-                        <img src={Player2}></img>
-                        <button>Manage team &gt;</button>
-                    </div>
-                    <h4>Vs.</h4>
-                    <div>
-                        <img src={Player3}></img>
-                        <img src={Player4}></img>
-                        <button>Manage team &gt;</button>
-                    </div>
-                </div>
-                <div id='teamsHolder'>
-                    <div id='teamOne' class="teamsGrid">
-                        <span width='50px'></span>
-                        <h4>GF</h4>
-                        <h4>GA</h4>
-                        <Team id='TeamOne' player1ImgSrc={Player1} player2ImgSrc={Player2}/>
-                    </div>
-                    <div id='teamTwo' class="teamsGrid">
-                        <h4>GF</h4>
-                        <h4>GA</h4>
-                        <span width='50px'></span>
-                        <Team id='TeamTwo' player1ImgSrc={Player3} player2ImgSrc={Player4}/>
-                    </div>
-                </div>
-                <div class='buttonHolder'>
-                    <button onClick={sendMatch}>Add</button>
-                </div>
-            </div>
-        </ReactModal>
-    );*/
-};
-
-function enforce_maxlength(event) {
-    var t = event.target;
-    if (t.hasAttribute('maxlength')) {
-        t.value = t.value.slice(0, t.getAttribute('maxlength'));
-    }
-}
-document.body.addEventListener('input', enforce_maxlength);
-
-function updateTotalScore(){
-    teamOnePoints = getNumberVal('player1GF') + getNumberVal('player2GF');
-    document.getElementById('teamOnePoints').innerHTML = teamOnePoints;
-    teamTwoPoints = getNumberVal('player3GF') + getNumberVal('player4GF')
-    document.getElementById('teamTwoPoints').innerHTML = teamTwoPoints;
-}
-document.body.addEventListener('input', updateTotalScore);
-
-function getNumberVal(elementId){
-    return parseInt(document.getElementById(elementId).value, 10) || 0;
+    )
 }
 
-function sendMatch(){
-    var i = 0;
-    var players = [];
-    selectedUsers.forEach(element => {
-        i++;
-        var GF = getNumberVal('player'+i+'GF');
-        var GA = getNumberVal('player'+i+'GA');
-        var player = 'playerId/":'+element+',/"goalsFrom/":'+GF+',/"goalsAgainst/":'+GA;
-        players.push(player);
-    });
-    console.log(players)
-}
+const mapStateToProps = ({ players }) => ({
+    players: players.list
+})
 
-export default AddTeam
+export default connect(mapStateToProps, {
+    getPlayers
+})(AddTeam)

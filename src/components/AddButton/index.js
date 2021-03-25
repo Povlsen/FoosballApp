a6dfa7df7a51b8d2c5a96f264e6c306c87ad85a2
Nikolai R. T. Povlsen <nikolaipovlsen@yahoo.dk>
2021-03-25 15:21:45 +0100

    ModalOpen button responds to theme

--- src/components/AddButton/index.js
+++ src/components/AddButton/index.js
@@ -1,21 +1,32 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import plus from '../../assets/plus.svg'
+
+import { connect } from 'react-redux'
+import  { setReduxState } from '../../actions/SetReduxState'
 
 import AddTeam from '../AddMatch'
 import './AddButton.scss'
 
-const AddButton = _ => {
+const AddButton = themeColor => {
     const [showAddTeamModal, setShowAddTeamModal] = useState(false)
-    
+    useEffect(_ => {
+    }, [themeColor])
+
     return (
         <div>
             <div class="add-button">
                 <AddTeam show={showAddTeamModal} handleClose={_ => setShowAddTeamModal(false)} />
-                <button onClick={_ => setShowAddTeamModal(true)}>
+                <button class={themeColor.themeColor} onClick={_ => setShowAddTeamModal(true)}>
                     <img src={plus} />
                 </button>
             </div>
         </div>
     )
 }
-export default AddButton
+const mapStateToProps = ({ global }) => ({
+    themeColor: global.themeColor
+})
+
+export default connect(mapStateToProps, {
+    setReduxState
+})(AddButton)
